06572188ac296fb4590e662c5056022d07222e36
Matt Goo <mattgoo@gmail.com>
2016-07-08 15:13:40 -0700

    changed app path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const appPath = `../kathyHuang/bin/static`;
+const appPath = `../kHuang`;
 const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
