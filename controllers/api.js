4f1bca4530bb6307c141625fb603f02f5d465dca
Matt Goo <mattgoo@gmail.com>
2016-07-20 23:07:30 -0700

    added other image sizes

--- controllers/api.js
+++ controllers/api.js
@@ -4,10 +4,12 @@ module.exports.getFlickrPhotos = (req, res) => {
   flickr.getPhotos().then(data => {
     for(var i=0; i<data.photos.photo.length; i++) {
       var obj = data.photos.photo[i];
-      var smallUrl = 'https://farm'+ obj.farm +'.staticflickr.com/'+ obj.server +'/'+ obj.id +'_'+ obj.secret +'_n	.jpg';
-      var medUrl = 'https://farm'+ obj.farm +'.staticflickr.com/'+ obj.server +'/'+ obj.id +'_'+ obj.secret +'_z	.jpg';
+      var smallUrl = 'https://farm'+ obj.farm +'.staticflickr.com/'+ obj.server +'/'+ obj.id +'_'+ obj.secret +'_n.jpg';
+      var medUrl = 'https://farm'+ obj.farm +'.staticflickr.com/'+ obj.server +'/'+ obj.id +'_'+ obj.secret +'_z.jpg';
+      var largeUrl = 'https://farm'+ obj.farm +'.staticflickr.com/'+ obj.server +'/'+ obj.id +'_'+ obj.secret +'_b.jpg';
       data.photos.photo[i].thumbnailUrl = smallUrl;
       data.photos.photo[i].mediumUrl = medUrl;
+      data.photos.photo[i].largeUrl = largeUrl;
     }
     res.send(data);
   });
